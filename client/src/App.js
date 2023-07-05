28bf5ef6f459d4a66ec670560d7afee0ed131e65
Abul Kibria <ahkibria@hotmail.co.uk>
2023-07-05 21:49:29 +0100

    done initial css for user interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,45 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import PatientCreate from "./components/patientCreate.js";
 import "./App.css";
+import "./grid.css";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
   const [patients, setPatients] = useState([]);
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/patients">Create Patient</Link>
-          </li>
-          <li>
-            <Link to="/foodchart">Patient Food Intake</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route
-          path="/patients"
-          element={
-            <PatientCreate patients={patients} setPatients={setPatients} />
-          }
-        />
-      </Routes>
-    </div>
+    <container className="grid-container">
+      <div className="header">Care Home</div>
+      <div className="sidebar">
+        <h1 style={{ fontsize: 20 }}>Content</h1>
+        <div className="sidebar-header">
+          <div className="sidebar-menu" onClick={() => navigate("/")}>
+            Home
+          </div>
+          <div className="sidebar-menu" onClick={() => navigate("/patients")}>
+            Create Patient
+          </div>
+          <div className="sidebar-menu" onClick={() => navigate("/foodIntake")}>
+            Food Intake
+          </div>
+        </div>
+      </div>
+
+      <div className="main">
+        Main
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route
+            path="/patients"
+            element={
+              <PatientCreate patients={patients} setPatients={setPatients} />
+            }
+          />
+        </Routes>
+      </div>
+      <div className="footer">Footer</div>
+    </container>
   );
 }
 
